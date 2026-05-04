2ba4e1a11b45a86e93cbd5e397ed5e932b1ab024
Nicholas <barrn06@yahoo.com>
2026-05-04 15:36:37 -0400

    Add HTTP CORS handling for division member placeholder function

--- firebase/src/index.ts
+++ firebase/src/index.ts
@@ -14,5 +14,6 @@ export {
   joinDivisionByCode,
   addPlayerToDivisionByEmail,
   addDivisionMemberPlaceholder,
+  addDivisionMemberPlaceholderHttp,
   mergeDivisionPlayerRecords,
 } from './divisions/divisionFunctions';
